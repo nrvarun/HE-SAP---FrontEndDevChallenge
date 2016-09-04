1626bbfc2752a721090967ebd27031da00415c5b
nrvarun <nr.varun79@gmail.com>
2016-09-04 15:41:09 +0530

    angular added

--- gulpfile.js
+++ gulpfile.js
@@ -13,28 +13,26 @@ var paths = {
      html           : ['./app/**/*.html'],
      sass           : ['./app/scss/*.scss'],
      javascript     : [
-                         './app/**/*.js',
+                         './app/js/*.js',
+                         './app/js/app.*.js',
                          '!./app/js/app.js',
                          '!./app/lib/**'
                       ],
      css            : './app/css/*.css'
  };
 
-gulp.task('default',['watch','useref','sass'], function(){
+gulp.task('default',['watch','sass'], function(){
 	gutil.log("Gulp is running");
 });
 
-gulp.task('index', function(){
-     return gulp.src('./app/index.html')
-         .pipe(inject(
-             gulp.src(paths.javascript,
-                 {read: false}), {relative: true}))
-         .pipe(gulp.dest('./app'))
-         .pipe(inject(
-             gulp.src(paths.css,
-             {read: false}), {relative: true}))
-         .pipe(gulp.dest('./app'));
- });
+/*gulp.task('index', function () {
+  var target = gulp.src('./app/index.html');
+
+  var sources = gulp.src(['./app/js/*.js','./css/*.css'], {read: false});
+
+  return target.pipe(inject(sources))
+               .pipe(gulp.dest('./app'));
+  });*/
 
 gulp.task('browserSync', function() {
   browserSync.init({
@@ -58,10 +56,4 @@ gulp.task('watch',['browserSync'],function(){
 	gulp.watch(paths.sass,['sass']);
 	gulp.watch(paths.html, browserSync.reload); 
   gulp.watch(paths.javascript,browserSync.reload); 
-});
-
-gulp.task('useref',function(){
-  return gulp.src('./app/index.html')
-             .pipe(useref())
-             .pipe(gulp.dest('./dist'));
 });
