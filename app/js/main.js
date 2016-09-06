9b9bea93a069cea4e486ae341848e9583f63b924
nrvarun <nr.varun79@gmail.com>
2016-09-07 03:54:24 +0530

    app made completely responsive

---
+++ app/js/main.js
@@ -0,0 +1,8 @@
+/*var div = document.querySelector('.home__search__results');
+if( div.scrollHeight - div.scrollTop === div.clientHeight ) {
+	alert('viola');
+}*/
+
+var langInput = document.querySelector('.searchLangField');
+
+console.log(document.querySelector('.searchLangField').value);
