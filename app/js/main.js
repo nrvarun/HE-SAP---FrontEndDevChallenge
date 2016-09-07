a1c3a512e9a139eaaa62050b847b70168ec6c8f7
nrvarun <nr.varun79@gmail.com>
2016-09-07 21:48:50 +0530

    minor changes

--- app/js/main.js
+++
@@ -1,8 +0,0 @@
-/*var div = document.querySelector('.home__search__results');
-if( div.scrollHeight - div.scrollTop === div.clientHeight ) {
-	alert('viola');
-}*/
-
-var langInput = document.querySelector('.searchLangField');
-
-console.log(document.querySelector('.searchLangField').value);
