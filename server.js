d39425beec1460047e81341d58401b9265466bab
nrvarun <nr.varun79@gmail.com>
2016-09-13 09:54:11 +0530

    server changed

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-var express = require('express');
-var app = express();
+var path 	=	require('path');
+var express = 	require('express');
+var app 	= 	express();
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
 
 // make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + './app'));
+app.use(express.static(path.join(__dirname + '/dist')));
 
 // set the home page route
 app.get('/', function(req, res) {
