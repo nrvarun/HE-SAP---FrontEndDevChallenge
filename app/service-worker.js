15d925ce0ed0a600d65d1200cc6f01a0c3605250
nrvarun <nr.varun79@gmail.com>
2017-02-14 14:19:56 +0530

    service worker updation

--- app/service-worker.js
+++ app/service-worker.js
@@ -3,12 +3,23 @@ console.log('Service worker has started');
 let CACHE_NAME = 'Topgit-v1';
 let urlsToCache = [
   '/',
+  'favicon.ico',
   '/css/style.css',
   '/js/bundle.js',
   '/images/bgimg.png',
   '/images/git404.png',
+  'manifest.json',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.woff2?v=4.7.0'
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.woff2?v=4.7.0',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://code.getmdl.io/1.2.0/material.brown-red.min.css',
+  'https://code.getmdl.io/1.2.0/material.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.8/angular.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.8/angular-route.min.js',
+  'https://gitsearch-ebecb.firebaseapp.com/js/app.js',
+  'https://gitsearch-ebecb.firebaseapp.com/js/home.controller.js',
+  'https://gitsearch-ebecb.firebaseapp.com/js/appConfig.js',
+  'https://gitsearch-ebecb.firebaseapp.com/js/main.controller.js'
 ];
 
 self.addEventListener('install', function (event) {
